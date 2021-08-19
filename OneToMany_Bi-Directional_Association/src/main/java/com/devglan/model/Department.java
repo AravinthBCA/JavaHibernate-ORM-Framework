e648620dd8f76f5113e14c157ac6bdc907ab7f6b
AravinthBCA <aravinthbca2001work@gmail.com>
2021-08-19 22:18:40 +0530

    OneToMany_Bi-Directional_Association

---
+++ OneToMany_Bi-Directional_Association/src/main/java/com/devglan/model/Department.java
@@ -0,0 +1,54 @@
+package com.devglan.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DEPARTMENTS_OTM_Bi")
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "RECORD_ID")
+	private Integer id;
+
+	@Column(name = "NAME")
+	private String name;
+//	@OneToMany(cascade = CascadeType.ALL) // It creates One common table 
+	                                      // it contains primary key values of both tables.
+	@OneToMany(mappedBy = "dept") // It creates only two tables (Bi-Directional)
+	private List<Employee> emps;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Employee> getEmps() {
+		return emps;
+	}
+
+	public void setEmps(List<Employee> emps) {
+		this.emps = emps;
+	}
+}
